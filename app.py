74c371930f278c9016f73c42ef5790240492146f
DataCodeCN <datacode.cn@gmail.com>
2026-04-18 18:49:05 -0300

    ajustes de verticais

--- app.py
+++ app.py
@@ -6,22 +6,6 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/theron-capital')
-def theron_capital():
-    return render_template('capital.html')
-
-@app.route('/theronseg')
-def theronseg():
-    return render_template('seg.html')
-
-@app.route('/theron-tour')
-def theron_tour():
-    return render_template('tour.html')
-
-@app.route('/theron-consultoria')
-def theron_consultoria():
-    return render_template('consultoria.html')
-
 @app.errorhandler(404)
 def page_not_found(e):
     return redirect(url_for('index'))
